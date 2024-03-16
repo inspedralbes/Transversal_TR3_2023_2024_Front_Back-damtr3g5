604a4d072099ca2550be281ed020550ad335ed87
osczapmar <a22osczapmar@inspedralbes.cat>
2024-03-16 19:10:50 +0100

    socket upgrade

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 const fs = require('fs');
 require('dotenv').config();
 const path = require('path');
-const multer = require('multer');
-const controladora = require('./controladora.js');
-const utils = require('./utils.js');
+//const multer = require('multer');
+//const controladora = require('./controladora.js');
+//const utils = require('./utils.js');
 const corsOptions = {
     origin: ["http://localhost:3000"],
     credentials: true,
@@ -18,8 +18,8 @@ const corsOptions = {
 };
 
 const app = express();
-const port = 8080
-const SERVER_URL = "localhost";
+const port = process.env.PORT;
+const SERVER_URL = process.env.SERVER;
 const sessionMiddleware = require('./sessionMiddleware.js');
 
 
@@ -27,18 +27,21 @@ app.use(cors(corsOptions));
 app.use(sessionMiddleware);
 app.use(bodyParser.json());
 app.use(express.json())
-app.use(express.static('images'))
-const server = http.createServer(app);
+app.use(express.static('images'));
 const { initializeSocket} = require("./sockets.js");
 
-initializeSocket(server,{ cors: corsOptions });
+const server = http.createServer(app);
 
 
 server.listen(port, () => {
     console.log(`Server listening at ${SERVER_URL}:${port}`);
 });
 
+initializeSocket(server);
+
+
 /*--Gestion de imagenes--*/
+/*
 app.get("/imagen/:name/:nombreArchivo", (req, res) => {
     const fileName = req.params.nombreArchivo;
     const folder = req.params.name;
@@ -60,9 +63,10 @@ app.post('/addskin', upload.single('file'), async (req, res) => {
         controladora.addCharacterSkin(idPersonaje, subida.name);
 });
 /*--Gestion de imagenes--*/
-
+/*
 app.get("/getsprites", (req, res) => {
     const filePath = path.join(__dirname, 'skins');
     const imageNames = utils.getImageNames(filePath);
     res.send(imageNames);
 });
+*/
--- Server/sockets.js
+++ Server/sockets.js
@@ -1,23 +1,53 @@
 const WebSocket = require('ws');
 
 
-function initializeSocket(server, cors) {
-    const wss = new WebSocket.Server({ server});
-    wss.on('listening', ()=> {
-        console.log("Server is listening on port "+8080)
-    })
-    wss.on('connection', (ws) => {
-        ws.on('message', (data)=> {
-            console.log('data received %o'+data)
-            ws.send(data);
-        })
-        ws.on('close', () => {
-            console.log('Client disconnected');
-        });
-    });
-    
+function initializeSocket(server) {
+    const wss = new WebSocket.Server({server});
 
-    
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received: %s', message);
+
+        // Handle incoming messages here
+        handleMessage(message);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+}
+
+
+function handleMessage(message) {
+    // Implement logic to handle different types of messages
+    // Example: parse JSON message and handle different message types
+    try {
+        const data = JSON.parse(message);
+        switch (data.type) {
+            case 'movement':
+                handleMovement(data);
+                break;
+            case 'action':
+                handleAction(data);
+                break;
+            // Add more cases for different message types
+            default:
+                console.log('Unknown message type');
+        }
+    } catch (error) {
+        console.error('Error parsing message:', error);
+    }
+}
+
+function handleMovement(data) {
+    // Implement logic to handle player movement
+}
+
+function handleAction(data) {
+    // Implement logic to handle player actions
 }
 
 module.exports = { initializeSocket};
